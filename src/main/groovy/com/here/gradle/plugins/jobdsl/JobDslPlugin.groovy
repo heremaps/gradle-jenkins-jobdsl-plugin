96e920a83ca36db39bc5d6dd9625d77a00e957b4
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2016-01-29 16:04:10 +0100

    Change default source folder for DSL scripts to src/jobdsl
    Change-Id: Id4fabffc8b28ebf6ced78bbcea2f1aa765049fe1

--- src/main/groovy/com/here/gradle/plugins/jobdsl/JobDslPlugin.groovy
+++ src/main/groovy/com/here/gradle/plugins/jobdsl/JobDslPlugin.groovy
@@ -24,7 +24,7 @@ class JobDslPlugin implements Plugin<Project> {
         project.sourceSets {
             jobdsl {
                 groovy {
-                    srcDirs 'jobdsl'
+                    srcDirs 'src/jobdsl'
                 }
             }
         }
