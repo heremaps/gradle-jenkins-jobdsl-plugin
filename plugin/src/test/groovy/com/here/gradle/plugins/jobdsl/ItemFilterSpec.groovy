5df2a3fac029d0fd25aded35048e2e4682993e84
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2016-04-14 17:34:25 +0200

    Add unit tests for ItemFilter
    Change-Id: I2fa528642aa080d362d8157f00f382a4e8686702

---
+++ plugin/src/test/groovy/com/here/gradle/plugins/jobdsl/ItemFilterSpec.groovy
@@ -0,0 +1,35 @@
+package com.here.gradle.plugins.jobdsl
+
+import spock.lang.Specification
+
+class ItemFilterSpec extends Specification {
+
+    def 'filter is ignored when null'() {
+        setup:
+        ItemFilter filter = new ItemFilter(null)
+
+        expect:
+        filter.matches('name') == true
+
+    }
+
+    def 'filter is ignored when empty'() {
+        setup:
+        ItemFilter filter = new ItemFilter('')
+
+        expect:
+        filter.matches('name') == true
+    }
+
+    def 'regular expression is working'() {
+        setup:
+        ItemFilter filter = new ItemFilter('^Test(Job|View)')
+
+        expect:
+        filter.matches('Test') == false
+        filter.matches(' TestJob') == false
+        filter.matches('TestJob') == true
+        filter.matches('TestView') == true
+    }
+
+}
