9b37e054e110cea346d9c793faad55adfeb3d648
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2016-06-29 16:07:04 +0200

    Add basic functional tests for the plugin
    Change-Id: Ie27b776799f585217cb75c98873bcddd4ac97950

---
+++ plugin/src/functionalTest/groovy/com/here/gradle/plugins/jobdsl/JobDslPluginTest.groovy
@@ -0,0 +1,57 @@
+package com.here.gradle.plugins.jobdsl
+
+import com.here.gradle.plugins.jobdsl.tasks.GenerateXmlTask
+import com.here.gradle.plugins.jobdsl.tasks.UpdateJenkinsTask
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import spock.lang.Specification
+
+class JobDslPluginTest extends Specification {
+
+    Project project
+
+    def setup() {
+        project = ProjectBuilder.builder().build()
+    }
+
+    def 'dslGenerateXml task is created'() {
+        when:
+        project.pluginManager.apply('com.here.jobdsl')
+
+        then:
+        project.tasks.dslGenerateXml instanceof GenerateXmlTask
+    }
+
+    def 'dslUpdateJenkins task is created'() {
+        when:
+        project.pluginManager.apply('com.here.jobdsl')
+
+        then:
+        project.tasks.dslUpdateJenkins instanceof UpdateJenkinsTask
+    }
+
+    def 'groovy plugin is applied'() {
+        when:
+        project.pluginManager.apply('com.here.jobdsl')
+
+        then:
+        project.plugins.hasPlugin('groovy')
+    }
+
+    def 'jobdsl source set is created'() {
+        when:
+        project.pluginManager.apply('com.here.jobdsl')
+
+        then:
+        project.sourceSets.findByName('jobdsl')
+    }
+
+    def 'jobdsl extension is created'() {
+        when:
+        project.pluginManager.apply('com.here.jobdsl')
+
+        then:
+        project.extensions.findByName('jobdsl')
+    }
+
+}
