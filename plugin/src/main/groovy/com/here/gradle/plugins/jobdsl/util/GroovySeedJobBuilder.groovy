b27703555fa61db1e7080b15eaf1b1a085f35d17
Martin Nonnenmacher <martin.nonnenmacher@here.com>
2016-08-17 16:41:33 +0200

    Add GroovySeedJobBuilder to generate seed jobs
    Add the GroovySeedJobBuilder that provides the configuration for seed jobs
    that run using a System Groovy Script build step. This is expected to be
    much faster than using the dslUpdateJenkins task in a seed job, because it
    uses the interal Jenkins API instead of the REST API.
    
    Change-Id: I62bd42e08605d25a68a7859c08195d741937ba6f

---
+++ plugin/src/main/groovy/com/here/gradle/plugins/jobdsl/util/GroovySeedJobBuilder.groovy
@@ -0,0 +1,53 @@
+package com.here.gradle.plugins.jobdsl.util
+
+import javaposse.jobdsl.dsl.DslFactory
+
+/**
+ * A {@link JobBuilder2} that creates a seed job that runs the dslGenerateXml Gradle task to create the XML files and
+ * then uses a system groovy script build step to update the jobs. This is a replacement for using the dslUpdateJenkins
+ * task in a seed job and promises a much better performance because it uses the internal Jenkins API instead of the
+ * REST API.<br>
+ * The builder does not provide a default way to get the files of the Gradle project, this has to be added by calling
+ * {@link JobBuilder2#addDsl(groovy.lang.Closure)}. Usually this will be a copy artifacts step or a SCM configuration.
+ */
+class GroovySeedJobBuilder extends JobBuilder2 {
+
+    /**
+     * Provide the script that will execute the dslGenerateXml Gradle task for your project. It defaults to the basic
+     * "./gradlew --no-daemon dslGenerateXml" which will only work if your project is present in the workspace directory
+     * and does not use server configuration.<br>
+     * Set this to null if you get the XML files in another way, for example by copying them from another job.
+     */
+    String generateXmlScript = './gradlew --no-daemon dslGenerateXml'
+
+    /**
+     * The base directory where the generated XML files are located. Usually this is the build/jobdsl/xml folder in the
+     * Gradle project. This value is used by the default {@link #seedJobGroovyScript}, if you change the script you have
+     * to include the string XML_BASE_DIR_PLACEHOLDER which will be replaced by xmlBaseDir automatically.
+     */
+    String xmlBaseDir = 'build/jobdsl/xml'
+
+    /**
+     * The Groovy script that updates the jobs from the XML files, executed in a System Groovy Script build step.<br>
+     * All items (folders and jobs) are compared and only updated when the XML has changed. Folders that contain views
+     * will always be updated.<br>
+     * Views in the Jenkins root will always be updated, views inside folders will always be re-created.<br>
+     * <em>Only change this value if you know what you are doing!</em>
+     */
+    String seedJobGroovyScript = getClass().getClassLoader().getResource('seedJobGroovyScript.groovy').text
+
+    GroovySeedJobBuilder(DslFactory dslFactory) {
+        super(dslFactory)
+
+        addDsl {
+            steps {
+                if (generateXmlScript) {
+                    shell(generateXmlScript)
+                }
+
+                systemGroovyCommand(seedJobGroovyScript.replaceAll('XML_BASE_DIR_PLACEHOLDER', xmlBaseDir))
+            }
+        }
+    }
+
+}
---
+++ plugin/src/main/resources/seedJobGroovyScript.groovy
@@ -0,0 +1,132 @@
+import hudson.FilePath
+import hudson.model.Item
+import hudson.model.ItemGroup
+import hudson.model.ModifiableViewGroup
+import hudson.model.View
+import jenkins.model.Jenkins
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import javax.xml.transform.stream.StreamSource
+import org.custommonkey.xmlunit.Diff
+import org.custommonkey.xmlunit.XMLUnit
+
+createdItems = 0
+createdViews = 0
+updatedItems = 0
+updatedViews = 0
+totalItems = 0
+totalViews = 0
+
+def updateItem(Item item, String name, FilePath file) {
+    println "  Update item ${name}"
+
+    try {
+        String oldConfig = item.getConfigFile().asString()
+        String newConfig = file.read().text
+        Diff diff = XMLUnit.compareXML(oldConfig, newConfig)
+        if (diff.identical()) {
+            println "  Item ${name} did not change"
+            return
+        }
+    } catch (Exception e) {
+        println "  WARNING: Could not create XML diff for ${name}"
+    }
+
+    def source = new StreamSource(file.read())
+    item.updateByXml(source)
+    updatedItems++
+}
+
+def createItem(ItemGroup parent, String name, FilePath file) {
+    println "  Create item ${name}"
+    if (parent instanceof ModifiableViewGroup) {
+        parent.createProjectFromXML(name, file.read())
+        createdItems++
+    } else {
+        println "  Could not create project in parent of type ${parent.class}"
+    }
+}
+
+def updateView(View view, String name, FilePath file) {
+    println "  Update view ${name}"
+    def source = new StreamSource(file.read())
+    view.updateByXml(source)
+    updatedViews++
+}
+
+def createView(ItemGroup parent, String name, FilePath file) {
+    println "  Create view ${name}"
+    if (parent instanceof ModifiableViewGroup) {
+        ((ModifiableViewGroup) parent).addView(View.createViewFromXML(name, file.read()))
+        createdViews++
+    } else {
+        println "  Could not create view in parent of type ${parent.class}"
+    }
+}
+
+def findParentItem(String fullName) {
+    Jenkins jenkins = Jenkins.getInstance()
+    int i = fullName.lastIndexOf('/')
+    switch (i) {
+        case -1:
+        case 0:
+            return jenkins
+        default:
+            def parentName = fullName.substring(0, i)
+            Item item = jenkins.getItemByFullName(parentName)
+            return item instanceof ItemGroup ? (ItemGroup) item : null
+    }
+}
+
+def updateJobs(FilePath filePath, String namePrefix = '') {
+    List<FilePath> directories = []
+    filePath.list().each { file ->
+        if (file.isDirectory()) {
+            directories << file
+        } else if (file.name.endsWith('.xml')) {
+            def name = file.name[0..-5]
+            Jenkins.checkGoodName(name)
+            def fullName = "${namePrefix}${name}"
+            println "Processing ${fullName}"
+            def reader = new BufferedReader(new InputStreamReader(file.read()))
+            def parent = findParentItem(fullName)
+            def firstLine = reader.readLine()
+            if (firstLine.contains('View')) {
+                totalViews++
+                def view = parent.getView(name)
+                if (view) {
+                    updateView(view, name, file)
+                } else {
+                    createView(parent, name, file)
+                }
+            } else {
+                totalItems++
+                def item = Jenkins.getInstance().getItemByFullName(fullName)
+                if (item) {
+                    updateItem(item, name, file)
+                } else {
+                    createItem(parent, name, file)
+                }
+            }
+        }
+    }
+
+    directories.each { dir ->
+        updateJobs(dir, "${namePrefix}${dir.name}/")
+    }
+}
+
+def workspace = build.workspace
+def xmlBase = new FilePath(workspace, 'XML_BASE_DIR_PLACEHOLDER')
+updateJobs(xmlBase)
+
+println """\
+            SEED JOB SUMMARY
+
+            Created items: ${createdItems}
+            Updated items: ${updatedItems}
+            Total items  : ${totalItems}
+
+            Created views: ${createdViews}
+            Updated views: ${updatedViews}
+            Total views  : ${totalViews}""".stripIndent()
